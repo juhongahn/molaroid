c05c000e392e45bcc4f7ad69f9f3fb8ab4c32628
juhongahn <juhong.ahn.dev@gmail.com>
2023-04-07 16:13:39 +0900

    test: 경로 수정후 파일 경로로부터 데이터 가져오기

--- src/pages/api/image/generate.js
+++ src/pages/api/image/generate.js
@@ -9,90 +9,123 @@ export const config = {
       bodyParser: false
     }
   }
-let postDirName;
-// multer 미들웨어 설정
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      // 이미지를 저장할 경로 지정
-      const postsDir = '/home/opc/moraloid/posts';
-      let dirNum = 1;
-      // 폴더의 마지막 숫자를 찾음
-      while (fs.existsSync(`${postsDir}/${dirNum}`)) {
-        dirNum++;
-      }
-      // 마지막 숫자보다 1 큰 경로를 만듦
-      const newDir = `${postsDir}/${dirNum}`;
-      postDirName = newDir;
-      fs.mkdirSync(newDir);
-      cb(null, newDir);
-    },
-    filename: function (req, file, cb) {
-      cb(null, 'input.jpg');
-    }
-});
+// let postDirName;
+// // multer 미들웨어 설정
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       // 이미지를 저장할 경로 지정
+//       const postsDir = '/home/opc/moraloid/posts';
+//       let dirNum = 1;
+//       // 폴더의 마지막 숫자를 찾음
+//       while (fs.existsSync(`${postsDir}/${dirNum}`)) {
+//         dirNum++;
+//       }
+//       // 마지막 숫자보다 1 큰 경로를 만듦
+//       const newDir = `${postsDir}/${dirNum}`;
+//       postDirName = newDir;
+//       fs.mkdirSync(newDir);
+//       cb(null, newDir);
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, 'input.jpg');
+//     }
+// });
   
-const upload = multer({ storage: storage });
+//const upload = multer({ storage: storage });
 
 export default async function handler(req, res) {
     
-    await new Promise((resolve, reject) => {
-        upload.single('image')(req, res, function (err) {
-          if (err) {
-            console.log(err.message)
-            res.status(400).json({ error: err.message });
-            reject(err);
-            return;
-          }
-          resolve();
-        });
-    });
+    // await new Promise((resolve, reject) => {
+    //     upload.single('image')(req, res, function (err) {
+    //       if (err) {
+    //         console.log(err.message)
+    //         res.status(400).json({ error: err.message });
+    //         reject(err);
+    //         return;
+    //       }
+    //       resolve();
+    //     });
+    // });
 
   // Python 파일 실행
-  const pythonProcess = spawn('python', ['/home/opc/moraloid/server/moraloidCore.py']);
+  //const pythonProcess = spawn('python', ['/home/opc/moraloid/server/moraloidCore.py']);
+  const postsDir = '/home/opc/moraloid/posts_test/1';
+  const boundary = 'myboundary';
+  res.setHeader('Content-Type', `multipart/mixed; boundary="${boundary}"`);
 
+  const inputImagePath = postsDir + '/input.jpg';
+  const outputAudioPath = postsDir + '/output.midi';
+  const outputTextPath = postsDir + '/output.txt';
+  
+  Promise.all([
+    fs.promises.readFile(inputImagePath),
+    fs.promises.readFile(outputTextPath),
+    fs.promises.readFile(outputAudioPath),
+  ]).then(([inputImage, outputText, outputAudio]) => {
+    const responseBody = [
+      'Content-Type: image/jpeg',
+      '',
+      inputImage,
+      `--${boundary}`,
+      'Content-Type: audio/mpeg',
+      '',
+      outputAudio,
+      `--${boundary}`,
+      'Content-Type: text/plain',
+      '',
+      outputText,
+      `--${boundary}--`
+    ].join('\r\n');
+
+    res.status(200).end(responseBody);
+  }).catch((err) => {
+    console.error(err);
+    res.statusCode = 500;
+    res.end();
+  });
+  
   // 자식 프로세스가 끝나면 콜백 호출
-  pythonProcess.on('exit', (code) => {
+  // pythonProcess.on('exit', (code) => {
     
-    const boundary = 'myboundary';
-    res.setHeader('Content-Type', `multipart/mixed; boundary="${boundary}"`);
-    const postsDir = '/home/opc/moraloid/posts_test/1';
-    const inputImagePath = postsDir + '/input.jpg';
-    const outputAudioPath = postsDir + '/output.midi';
-    const outputTextPath = postsDir + '/output.txt';
-    outputAudioPath
-    Promise.all([
-      fs.promises.readFile(inputImagePath),
-      fs.promises.readFile(outputTextPath),
-      fs.promises.readFile(outputAudioPath),
-    ]).then(([inputImage, outputText, outputAudio]) => {
-      const responseBody = [
-        'Content-Type: image/jpeg',
-        '',
-        inputImage,
-        `--${boundary}`,
-        'Content-Type: audio/mpeg',
-        '',
-        outputAudio,
-        `--${boundary}`,
-        'Content-Type: text/plain',
-        '',
-        outputText,
-        `--${boundary}--`
-      ].join('\r\n');
+  //   const boundary = 'myboundary';
+  //   res.setHeader('Content-Type', `multipart/mixed; boundary="${boundary}"`);
 
-      res.status(200).end(responseBody);
-    }).catch((err) => {
-      console.error(err);
-      res.statusCode = 500;
-      res.end();
-    });
-  })
-  pythonProcess.stderr.on('data', (data) => {
-      console.error(`stderr: ${data}`);
-  });
-  pythonProcess.on('close', (code) => {
-      console.log(`child process exited with code ${code}`);
-  });
+  //   const inputImagePath = postDirName + '/input.jpg';
+  //   const outputAudioPath = postDirName + '/output.midi';
+  //   const outputTextPath = postDirName + '/output.txt';
+  //   Promise.all([
+  //     fs.promises.readFile(inputImagePath),
+  //     fs.promises.readFile(outputTextPath),
+  //     fs.promises.readFile(outputAudioPath),
+  //   ]).then(([inputImage, outputText, outputAudio]) => {
+  //     const responseBody = [
+  //       'Content-Type: image/jpeg',
+  //       '',
+  //       inputImage,
+  //       `--${boundary}`,
+  //       'Content-Type: audio/mpeg',
+  //       '',
+  //       outputAudio,
+  //       `--${boundary}`,
+  //       'Content-Type: text/plain',
+  //       '',
+  //       outputText,
+  //       `--${boundary}--`
+  //     ].join('\r\n');
+
+  //     res.status(200).end(responseBody);
+  //   }).catch((err) => {
+  //     console.error(err);
+  //     res.statusCode = 500;
+  //     res.end();
+  //   });
+  // })
+  // pythonProcess.stderr.on('data', (data) => {
+  //     console.error(`stderr: ${data}`);
+  // });
+  // pythonProcess.on('close', (code) => {
+  //     console.log(`child process exited with code ${code}`);
+  // });
 
   
   
